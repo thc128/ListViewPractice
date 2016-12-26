7a7aa2b11a7a1774df0fd6bfd464a1ef43dcb0b0
thc128 <asaph141@gmail.com>
2016-12-26 20:02:02 +0200

    Create the project

---
+++ ListView/XML_Access.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.IO;
+
+namespace ListView
+{
+    class XML_Access
+    {
+        public static XElement DataRoot;
+        public static string DataPath = @"C:\Users\asaf\Desktop\תרגול בC++\ListView\ListView\data1.xml";
+        public XML_Access()
+        {
+            if(File.Exists(DataPath))
+            {
+                LoadData();
+            }
+            else
+            {
+                CreateFile();
+            }
+        }
+
+        void CreateFile()
+        {
+        }
+
+        void LoadData()
+        {
+            try
+            {
+                DataRoot = XElement.Load(DataPath);
+            }
+            catch
+            {
+                throw new Exception("Cannot open the file");
+            }
+        }
+    }
+}
+ 
